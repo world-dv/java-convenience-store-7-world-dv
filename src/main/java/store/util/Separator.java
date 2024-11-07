97926d2ac2d5d23a8bc72d855bc217a0e9e0fabe
world-dv <goodforx0147@naver.com>
2024-11-07 20:43:30 +0900

    feat: 개별 상품에서 상품명과 수량 분리
    - 개별 상품에서 상품명과 수량을 분리하기 위해 구분자(하이픈)를 기준으로 문자열을 분리합니다.

--- src/main/java/store/util/Separator.java
+++ src/main/java/store/util/Separator.java
@@ -7,4 +7,8 @@ public class Separator {
     public static List<String> splitItems(String items) {
         return List.of(items.split(","));
     }
+
+    public static List<String> splitItem(String item) {
+        return List.of(item.split("-"));
+    }
 }

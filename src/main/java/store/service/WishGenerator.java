05e304b0d87d59b3eabf4342be0d223fe0b40cc3
world-dv <goodforx0147@naver.com>
2024-11-08 01:39:42 +0900

    feat: 상품명 검증 로직 추가
    - 상품 목록에 있는 상품만 입력받기 위해 상품명을 검증하는 로직을 추가합니다.
    - 상품명이 상품 목록에 존재하지 않는다면 예외를 발생시킵니다.

--- src/main/java/store/service/WishGenerator.java
+++ src/main/java/store/service/WishGenerator.java
@@ -1,6 +1,8 @@
 package store.service;
 
+import java.util.HashMap;
 import java.util.List;
+import store.domain.Product;
 import store.domain.Wish;
 
 public class WishGenerator {
@@ -8,9 +10,16 @@ public class WishGenerator {
     private final String name;
     private final Integer amount;
 
-    public WishGenerator(List<String> item) {
+    public WishGenerator(HashMap<String, List<Product>> products, List<String> item) {
+        validateName(products, item.getFirst());
         this.name = item.getFirst();
         this.amount = validateNumber(item.getLast());
+    }
+
+    private void validateName(HashMap<String, List<Product>> products, String name) {
+        if (!products.containsKey(name)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private Integer validateNumber(String amount) {

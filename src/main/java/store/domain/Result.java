b8213a61d565928137fa57c8d26af13db27f3583
world-dv <goodforx0147@naver.com>
2024-11-09 17:53:37 +0900

    docs: 기능 구현 목록 갱신

--- src/main/java/store/domain/Result.java
+++ src/main/java/store/domain/Result.java
@@ -23,4 +23,40 @@ public class Result {
     public Pay getPromotionResult() {
         return promotionResult;
     }
+
+    public Integer calculateTotalAmount() {
+        Integer totalAmount = 0;
+        if (originalResult != null) {
+            totalAmount += originalResult.getTotalAmount();
+        }
+        if (promotionResult != null) {
+            totalAmount += promotionResult.getTotalAmount();
+        }
+        return totalAmount;
+    }
+
+    public Integer calculateTotalPrice() {
+        Integer totalPrice = 0;
+        if (originalResult != null) {
+            totalPrice += originalResult.getTotalPrice();
+        }
+        if (promotionResult != null) {
+            totalPrice += promotionResult.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
+    public Integer calculateFreeAmount() {
+        if (promotionResult != null) {
+            return promotionResult.getFreeAmount();
+        }
+        return 0;
+    }
+
+    public Integer calculateFreePrice() {
+        if (promotionResult != null) {
+            return promotionResult.getFreeAmount() * promotionResult.getPrice();
+        }
+        return 0;
+    }
 }

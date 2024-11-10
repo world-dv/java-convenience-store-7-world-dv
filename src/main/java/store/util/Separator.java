c592daec3a607da0c07468d3f2ec64af5cd2ab70
world-dv <goodforx0147@naver.com>
2024-11-11 06:00:18 +0900

    refactor: split 제한 설정

--- src/main/java/store/util/Separator.java
+++ src/main/java/store/util/Separator.java
@@ -6,10 +6,11 @@ public class Separator {
 
     private static final String ITEMS_SEPARATOR = ",";
     private static final String ITEM_SEPARATOR = "-";
+    private static final Integer ITEMS_SEPARATION_LIMIT = -1;
     private static final Integer ITEM_SEPARATION_LIMIT = 2;
 
     public static List<String> splitItems(String items) {
-        return List.of(items.split(ITEMS_SEPARATOR));
+        return List.of(items.split(ITEMS_SEPARATOR, ITEMS_SEPARATION_LIMIT));
     }
 
     public static List<String> splitItem(String item) {
